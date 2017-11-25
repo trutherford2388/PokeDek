7252b8bad4719bede0a7b68232c9cafce070d74a
Travis L Rutherford <trutherford2388@icloud.com>
2017-11-25 06:51:05 -0600

    Add modal features to details icon

--- app/poke-list/list-pokemons.component.ts
+++ app/poke-list/list-pokemons.component.ts
@@ -1,42 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Pokemon } from '../shared/pokemon';
 import { PokemonService } from '../shared/pokemon.service';
-import { error } from 'util';
+import { ModalDirective } from 'ng2-bootstrap';
 
 @Component({
-    moduleId: module.id,
-    selector: 'pk-list',
-    templateUrl: 'list-pokemons.template.html'
+  moduleId: module.id,
+  selector: 'pk-list',
+  templateUrl: 'list-pokemons.template.html',
+    styles: [`
+          :host >>> .tooltip-inner {
+          background-color: #FF7768;
+          color: #fff;
+          }
+          :host >>> .tooltip .tooltip-arrow {
+          border-bottom-color: #FF7768;
+          }
+  `]
 })
 
 export class ListPokemonsComponent implements OnInit {
-    pokemon: Pokemon[];
-    errorMessage: string;
+  @ViewChild('childModal') public childModal:ModalDirective;
+  pokemon: Pokemon[];
+  errorMessage: string;
 
-    constructor(private _pokemonService: PokemonService) {}
+  // Modal Properties
+  selectedPokemonLoaded: boolean = false;
+  pokeDetails: Pokemon;
 
-    ngOnInit() {
-        // get all Pokemon
-        this.getPokemons();
-    }
+  constructor(private _pokemonService: PokemonService) { }
 
-    getPokemons() {
-        this._pokemonService.getPokemons().subscribe(
+  ngOnInit() {
+    // get all Pokemons
+    this.getPokemons();
+  }
+
+  public hideChildModal() {
+    this.childModal.hide();
+  }
+
+  getPokemons() {
+    this._pokemonService.getPokemons()
+          .subscribe(
             (pokemon: Pokemon[]) => {
-                this.pokemon = pokemon;
+              console.log(pokemon);
+              this.pokemon = pokemon;
             },
             error => this.errorMessage = <any> error
-        );
-    }
-
-    deletePokemon(pokemon: Pokemon) {
-        this._pokemonService.deletePokemon(pokemon)
-            .subscribe(
-                () => {},
-                error => this.errorMessage = <any> error,
-                () => {
-                    this.getPokemons();
-                }
             );
+  }
+
+  deletePokemon(pokemon: Pokemon) {
+    this._pokemonService.deletePokemon(pokemon)
+          .subscribe(
+            () => this.deletePokemonFromList(pokemon),
+            error => this.errorMessage = <any> error,
+            );
+  }
+
+  viewSinglePokemon(id: number) {
+    this._pokemonService.getPokemonDetails(id)
+          .subscribe(
+            (pokemon: Pokemon) => {
+              this.pokeDetails = pokemon;
+              this.selectedPokemonLoaded = true;
+              this.childModal.show();
+            },
+            error => this.errorMessage = <any> error
+            )
+  }
+
+  private deletePokemonFromList(pokemon: Pokemon) {
+    var index = this.pokemon.indexOf(pokemon, 0);
+    if(index > -1) {
+      this.pokemon.splice(index, 1);
     }
+  }
+
 }
